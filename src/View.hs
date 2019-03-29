1f18b5ea454a22ad2efb3b2a835066d0d7b838fb
u6826541 <u6826541@anu.edu.au>
2019-03-29 22:42:47 +1100

    assignment1 part A

--- src/View.hs
+++ src/View.hs
@@ -50,8 +50,16 @@ colourNameToColour colour = case colour of
 shapeToPicture :: Shape -> Picture
 shapeToPicture shape = case shape of
     Line (a,b) (c,d)->(polyline [(a,b),(c,d)]) & coordinatePlane
-    Polygon [(a,b)] -> solidPolygon [(a,b)]
-    Rectangle (a,b) (c,d) -> (solidRectangle (abs (a-c)) (abs (b-d)))& coordinatePlane
-    Circle (a,b) (c,d) -> (solidCircle (sqrt ((a-c)**2+(b-d)**2)))& coordinatePlane
+
+    Polygon (f:s:t:xs) -> (solidPolygon (f:s:t:xs)) & coordinatePlane
+    Polygon [_, _] -> error "just two points,not enough!"
+    Polygon [_] -> error "just one point,not enough!"
+    Polygon [] -> error "no points in the list"
+
+    Rectangle (a,b) (c,d) -> (translated ((a+c)/2) ((b+d)/2) (solidRectangle (abs (a-c)) (abs (b-d))))& coordinatePlane
+
+    Circle (a,b) (c,d) -> (translated a b (solidCircle (sqrt ((a-c)**2+(b-d)**2))))& coordinatePlane
+
     Ellipse (a,b) (c,d) -> solidCircle (sqrt ((a-c)**2+(b-d)**2))
+
 
